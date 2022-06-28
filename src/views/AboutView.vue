d81097d001d812cf3cb21c33c94ab5a6e52de5df
Rana Fatrika <ranafatrika@gmail.com>
2022-06-28 12:17:04 +0700

    pweb

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,31 @@
 <script setup>
 import Navbar from "../components/Navbar.vue";
+import { onMounted } from "vue";
 
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundImage ="url('src/assets/Cover1.png')";
+  document.getElementsByTagName("body")[0].style.backgroundSize = "cover";
+});
 </script>
 <template>
   <Navbar/>
+  <div class="judul">
+    <h1 class="h1">Created By</h1>
+  </div>
+  <div class="box-container">
+    <div class="box1"> <div class="card" style="width: 18rem;">
+      <img src="../assets/card1.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Aidil Zikri</p>
+      </div>
+    </div></div>
+    <div class="box2"> <div class="card" style="width: 15rem;">
+      <img src="../assets/card.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Rana Fatrika</p>
+      </div>
+    </div></div>
+  </div>
 
 </template>
 <script>
@@ -13,5 +35,36 @@ export default {
 </script>
 
 <style scoped>
+.card{
+  margin-top: 15rem;
+  margin-left: 5rem;
+}
+.card1{
+  margin-top: 10rem;
+  margin-left: 25rem;
+}
+.judul{
+  color: #090808;
+  font-style: oblique;
+  font-family: "poppins";
+  margin-top: 5rem;
+  text-align: center;
+}
+.box1, .box2{
+  width: 300px;
+  height: 100px;
+  margin-top: 5rem;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-right: 30rem;
+}
+
+.box-container{
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5rem;
+  margin-bottom: 5rem;
+
+}
 
 </style>

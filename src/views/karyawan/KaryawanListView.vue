a8852fceb529aa0be5e8550c4937e9b8d01dc6bb
Rana Fatrika <ranafatrika@gmail.com>
2022-06-28 09:04:51 +0700

    pweb

--- src/views/karyawan/KaryawanListView.vue
+++ src/views/karyawan/KaryawanListView.vue
@@ -1,4 +1,47 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  document.getElementsByTagName("body")[0].style.backgroundImage ="url('src/assets/Cover1.png')";
+  document.getElementsByTagName("body")[0].style.backgroundSize = "cover";
+});
+</script>
 <script>
 export default {
   name: "KaryawanListlView",}
 </script>
+<template>
+  <section class="hero is-small">
+    <div class="hero-body">
+      <p class="title">
+        Data Karyawan
+      </p>
+      <p class="subtitle">
+        Menampilkan Data Karyawan dan CRUD API endpoint
+      </p>
+    </div>
+  </section>
+
+  <section class="content">
+    <div class="container">
+      <button class="button mb-3 bg-primary" @click="showAdd">
+        <span class="icon is-small">
+          <i class="fas fa-plus"></i>
+        </span>
+        <span>Create</span>
+      </button>
+      <table class="table">
+        <thead class="table-name">
+        <tr>
+          <th>ID KARYAWAN</th>
+          <th>NAMA KARYAWAN</th>
+          <th>TANGGAL LAHIR</th>
+          <th>ALAMAT</th>
+          <th>NO HP</th>
+          <th>JABATAN</th>
+        </tr>
+        </thead>
+      </table>
+    </div>
+  </section>
+</template>
--- src/views/penjualan/PenjualanListView.vue
+++ src/views/penjualan/PenjualanListView.vue
@@ -6,6 +6,42 @@ onMounted(() => {
   document.getElementsByTagName("body")[0].style.backgroundSize = "cover";
 });
 </script>
+<template>
+  <section class="hero is-small">
+    <div class="hero-body">
+      <p class="title">
+        Data Penjualan
+      </p>
+      <p class="subtitle">
+        Menampilkan Data Penjualan dan CRUD API endpoint
+      </p>
+    </div>
+  </section>
+
+  <section class="content">
+    <div class="container">
+      <button class="button mb-3 bg-primary" @click="showAdd">
+        <span class="icon is-small">
+          <i class="fas fa-plus"></i>
+        </span>
+        <span>Create</span>
+      </button>
+      <table class="table">
+        <thead class="table-name">
+        <tr>
+          <th>NO NOTA</th>
+          <th>TANGGAL PENJUALAN</th>
+          <th>JUMLAH BARANG</th>
+          <th>ID PEMBELI</th>
+          <th>ID KARYAWAN</th>
+          <th>ID BARANG</th>
+        </tr>
+        </thead>
+      </table>
+    </div>
+  </section>
+</template>
+
 <script>
 export default {
   name: "PenjualanListView",}
